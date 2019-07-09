f8af1b4e147dad252f109a069545dc80b5e4b987
Luke Herrmann <herrmannluke@gmail.com>
2019-07-09 14:41:57 -0600

    new camper section text update

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,29 +8,35 @@ const IndexPage = () => (
     <div className="body-container">
       <div className="new-campers">
         <div className="container">
-          <div className="row">
-            <div className="col-xs-12 col-sm-2">
-              <div className="camper-sm-text">NOW ACCEPTING</div>
+          <div className="camper-text-container">
+            <div className="row">
+              <div className="col-xs-12 col-sm-2">
+                <div className="camper-sm-text">NOW ACCEPTING</div>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-xs-12 col-sm-4">
+                <div className="camper-lg-text">New Campers</div>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-xs-12 col-sm-4">
+                <div className="camper-sm-text">Creative Techology Camp</div>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-xs-12 col-sm-4">
+                <div className="camper-sm-text">
+                  Classes in apps, web, and VR
+                </div>
+              </div>
             </div>
           </div>
           <div className="row">
             <div className="col-xs-12 col-sm-4">
-              <div className="camper-lg-text">New Campers</div>
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-xs-12 col-sm-4">
-              <div className="camper-sm-text">Creative Techology Camp</div>
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-xs-12 col-sm-4">
-              <div className="camper-sm-text">Classes in apps, web, and VR</div>
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-xs-12 col-sm-4">
-              <div className="enroll-button">Enroll</div>
+              <div className="enroll-btn">
+                <div className="enroll-text">Enroll</div>
+              </div>
             </div>
           </div>
         </div>
